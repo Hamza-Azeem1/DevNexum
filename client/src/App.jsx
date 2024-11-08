bbdee138190f12bcd347dbe5000b811558eeb152
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2024-11-08 17:00:02 +0500

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,23 @@
-function App() {
+import logo from '../public/logo.png'
 
+function App() {
   return (
     <>
-      <div className='text-2xl font-serif'>
-        DevNexum
+      {/* Main container with gradient background */}
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-blue-700 to-black text-white">
+        {/* Logo in top-left corner */}
+        <div className="absolute -top-10 left-8">
+          <img src={logo} alt="DevNexum Logo" className="h-44 w-34" />
+        </div>
+
+        {/* Centered and styled DevNexum text */}
+        <div className="text-6xl font-bold font-arial tracking-widest drop-shadow-lg">
+          DevNexum
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
