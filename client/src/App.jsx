95cd15d1c2fff75765e5cfa002181b1cc5fb929d
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2024-11-08 17:33:09 +0500

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,14 +6,18 @@ function App() {
       {/* Main container with gradient background */}
       <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-black to-blue-700 text-white">
         {/* Logo in top-left corner */}
-        <div className="absolute -top-10 left-8">
+        <div className="absolute -top-12 -left-0">
           <img src={logo} alt="DevNexum Logo" className="h-44 w-34" />
         </div>
 
         {/* Centered and styled DevNexum text */}
         <div className="text-6xl font-bold font-arial tracking-widest drop-shadow-lg">
-          DevNexum
+          Denexum
         </div>
+        <div className="text-2xl mt-4 text-gray-300 font-light">
+          Crafting Digital Solutions, Empowering Your Vision.
+        </div>
+
       </div>
     </>
   );
